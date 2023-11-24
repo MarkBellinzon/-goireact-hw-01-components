d20da710ce1ee72848bd77a1876860480b963f1e
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-11-24 17:01:06 +0200

    friend_listitem

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -1,29 +1,59 @@
-import css from './Friends.module.css';
 import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+import css from './FriendList.module.css';
 
-export const FriendList = ({ friends }) => {
-  // console.log(friends);
 
-  return (
-    <div className={css.friends}>
-      <ul className={css.friends__list}>
-        {friends.map(({ avatar, name, isOnline, id }) => (
-          <li className={css.friends__item} key={id}>
-            <span className={`${css.friends__status} ${css[isOnline]}`}>
-              {isOnline}
-            </span>
-            <img className={css.avatar} src={avatar} alt={name} />
-            <p className={css.friends__name}>{name}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+export const FriendList = ({ friends }) => (
+  <ul className={css['friend-list']}>
+    {friends.map(({ id, avatar, name, isOnline }) => (
+      <FriendListItem
+        key={id}
+        avatar={avatar}
+        name={name}
+        isOnline={isOnline}
+      />
+    ))}
+  </ul>
+);
+
+FriendList.protoTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.number.isRequired }),
+  ).isRequired,
 };
 
-FriendList.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-  id: PropTypes.number.isRequired,
-};
+
+
+
+
+
+// import css from './FriendList.module.css';
+// import PropTypes from 'prop-types';
+
+
+// export const FriendList = ({ friends }) => {
+  
+//   return (
+//     <div className={css.friends}>
+//       <ul className={css.friends__list}>
+//         {friends.map(({ avatar, name, isOnline, id }) => (
+//           <li className={css.friends__item} key={id}>
+//             <span className={`${css.friends__status} ${css[isOnline]}`}>
+//               {isOnline}
+//             </span>
+//             <img className={css.avatar} src={avatar} alt={name} />
+//             <p className={css.friends__name}>{name}</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+        
+        
+// FriendList.propTypes = {
+//   avatar: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   isOnline: PropTypes.bool.isRequired,
+//   id: PropTypes.number.isRequired,
+// };
